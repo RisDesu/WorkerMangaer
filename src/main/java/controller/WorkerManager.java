8ccac32e62b53ba338d34d50fdcbfa7c65396bc1
RisDesu <phanhuy30012003@gmail.com>
2023-10-10 09:48:44 +0700

    Commit 1

---
+++ src/main/java/controller/WorkerManager.java
@@ -0,0 +1,41 @@
+package controller;
+
+import model.List;
+import view.Menu;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author HP
+ */
+public class WorkerManager extends Menu<String>{
+    private List list= new List();
+    public WorkerManager(String title, String[] s){
+        super(title, s);
+    }
+    
+    @Override
+    public void execute(int n) {
+        switch (n) {
+            case 1:
+                list.addWorker();
+                break;
+            case 2:
+                list.changeSalary(1);
+                break;
+            case 3:
+                list.changeSalary(2);
+                break;
+            case 4: 
+                list.printListHistory();
+                break;
+            case 5:
+                System.exit(0);        
+        }
+    }
+}
